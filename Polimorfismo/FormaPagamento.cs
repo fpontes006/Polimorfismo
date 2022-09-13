081c0be22ee3c32d63384f4e33f99807b44fc9ef
fpontes006 <fpontes006@gmail.com>
2022-09-13 14:11:08 -0300

    Adicionar arquivos de projeto.

---
+++ Polimorfismo/FormaPagamento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class FormaPagamento
+    {
+
+        public virtual void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado! ");
+        }
+    }
+
+    public class Boleto : FormaPagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado por boleto! ");
+        }
+    }
+
+    public class Pix : FormaPagamento
+    {
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado por Pix! ");
+        }
+    }
+
+    public class Cartao : FormaPagamento
+    {
+
+        public Cartao()
+        {
+            EfetuarPagamento();
+        }
+        public override void EfetuarPagamento()
+        {
+            Console.WriteLine("Pagamento efetuado por Cartão de Credito! ");
+        }
+    }
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Polimorfismo;
+
+Console.WriteLine("Digite três produtos a serem comprados: ");
+
+
+List<string> produtos = new List<string>();
+
+for (int i = 0; i < 3; i++)
+{
+    produtos.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Qual forma de pagamento voce deseja? (Pix,Boleto,Cartão): ");
+
+string formaPagamentoDesejada = Console.ReadLine();
+
+if (formaPagamentoDesejada == FormasPagamentoEnum.Boleto.ToString())
+{
+    Boleto boleto = new Boleto();
+    boleto.EfetuarPagamento();
+}
+else if (formaPagamentoDesejada == FormasPagamentoEnum.Pix.ToString())
+{
+    Pix pix = new Pix();
+    pix.EfetuarPagamento();
+}
+else if (formaPagamentoDesejada == FormasPagamentoEnum.Cartao.ToString())
+{
+    new Cartao();
+}
